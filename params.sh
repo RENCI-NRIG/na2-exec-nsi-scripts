a690ac04da1a5cfd7917fcf262db2a7f56cece20
Ilya Baldin <ibaldin@geni2.renci.org>
2018-07-26 09:55:00 -0400

    Automating time period

--- params.sh
+++ params.sh
@@ -3,5 +3,7 @@ GUID=urn:uuid:`uuidgen`
 SRC="es.net:2013::sunn-cr5:10_1_6:+#vlan=1779"
 DST="es.net:2013::chic-cr5:5_1_1:al2s#vlan=1779"
 BW="100"
-START="2018-07-26T20:00:00"
-END="2018-07-27T20:30:00"
+START=`date -u -v+1d "+%Y-%m-%dT%H:%M:%S"`
+END=`date -u -v+1d -v+30M "+%Y-%m-%dT%H:%M:%S"`
+#START="2018-07-26T20:00:00"
+#END="2018-07-27T20:30:00"
